58ee3dcff64726b9924b94a600e8c309fc282413
avinash <avinash@avinashs-MacBook-Pro.local>
2018-12-16 22:06:52 +0530

    round button added

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,5 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
+const RoundButtons = styled.div`
+  background-image: -webkit-linear-gradient(top, #f4f1ee, #fff);
+  background-image: linear-gradient(top, #f4f1ee, #fff);
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  position: relative;
+  &:after {
+    color: #e9e6e4;
+    content: "";
+    display: block;
+    font-size: 30px;
+    height: 30px;
+    text-decoration: none;
+    text-shadow: 0px -1px 1px #bdb5b4, 1px 1px 1px white;
+    position: absolute;
+    width: 30px;
+  }
+`;
+
+const TickButton = styled(RoundButtons)`
+  &:after {
+    content: "✔";
+    left: 23px;
+    top: 20px;
+  }
+`;
 export function TimePicker(props) {
-  return <input type="time" style={{ fontSize: "3em" }} />;
+  const currentDate = new Date();
+  const currentTime = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
+  return (
+    <div>
+      <input type="time" value={currentTime} style={{ fontSize: "3em" }} />
+      <TickButton val={"✔"} />
+    </div>
+  );
 }
